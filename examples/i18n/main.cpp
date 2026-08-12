5503734fa71ac396e4212ea9a892d95fe7e40b4c
floppyhammer <tannhauser_chen@outlook.com>
2026-08-12 16:21:30 +0800

    Improve i18 example

--- examples/i18n/main.cpp
+++ examples/i18n/main.cpp
@@ -1,9 +1,8 @@
-#include <resources/default_resource.h>
+#include <vecgui/app.h>
+#include <vecgui/resources/default_resource.h>
 
 #include <iostream>
 #include <random>
-
-#include "vecgui/app.h"
 
 using namespace vecgui;
 
@@ -17,12 +16,15 @@ class MyNode : public Node {
         margin_container->set_anchor_flag(AnchorFlag::FullRect);
         add_child(margin_container);
 
-        auto box_container = std::make_shared<HBoxContainer>();
-        box_container->set_separation(16);
-        margin_container->add_child(box_container);
+        auto font = Font::from_file("assets/unifont-17.0.03.otf");
+
+        auto s_container = std::make_shared<SplitContainer>();
+        s_container->set_separation(16);
+        margin_container->add_child(s_container);
 
         std::string text = "";
-        text += "👍😁😂\n";                      // Emoji
+        // text += "👍😁😂\n";                      // Emoji
+        text += "Hello world!\n";                  // English
         text += "你好世界！\n";                  // Chinese
         text += "こんにちは世界！\n";            // Japanese
         text += "안녕 세계\n";                   // Korean
@@ -41,22 +43,23 @@ class MyNode : public Node {
             auto label = std::make_shared<Label>();
             label->set_text(text);
             label->container_sizing.flag_h = ContainerSizingFlag::Fill;
-            // label->set_font(font);
+            label->set_font(font);
             label->set_font_size(32);
+            label->debug_box = StyleBox::simple_outline();
 
-            box_container->add_child(label);
+            s_container->add_child(label);
         }
 
         // Word wrapping.
         {
             auto label = std::make_shared<Label>();
             label->set_word_wrap(true);
-            label->set_custom_minimum_size(Vec2F(200, 600));
             label->set_text(text);
             label->container_sizing.flag_h = ContainerSizingFlag::Fill;
-            // label->set_font(font);
+            label->set_font(font);
+            label->debug_box = StyleBox::simple_outline();
 
-            box_container->add_child(label);
+            s_container->add_child(label);
         }
     }
 };
